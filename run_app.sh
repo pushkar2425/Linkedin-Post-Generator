d5ac52774b293347153fc65634a345bc17b8be94
PUSHKAR SINGH <pushkarjn10@gmail.com>
2025-08-01 02:07:25 +0530

    run_app.sh

--- run_app.sh
+++ run_app.sh
@@ -4,4 +4,4 @@
 source streamlit_env/bin/activate
 
 # Run the Streamlit application
-streamlit run main.py --server.headless true --server.port 8501
+streamlit run main.py --server.headless true --server.port 8504
