9a7eacbbd890dae65a1fb4696d4cbdc31e93cc61
PUSHKAR SINGH <pushkarjn10@gmail.com>
2025-08-01 02:38:56 +0530

    Delete run_app.bat

--- run_app.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Activate the virtual environment
-call streamlit_env\Scripts\activate.bat
-
-REM Run the Streamlit app
-streamlit run main.py --server.headless true --server.port 8504
